5ab8e2d7b68d94d2d2fc15e25b9e131aee218cab
KOIKOIMARU <k024g0108@g.neec.ac.jp>
2026-05-07 03:17:15 +0900

    01_05 オペレータ追加（頂点移動 + ICO生成）

--- level_editor.py
+++ level_editor.py
@@ -14,54 +14,83 @@ bl_info = {
     "category": "Object"
 }
 
-# ----------------------------
-# メニュー項目（前回のやつ）
-# ----------------------------
-def draw_menu_manual(self, context):
-    self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+# オペレータ：頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    # メニューを実行したときに呼ばれる処理
+    def execute(self, context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+        print("頂点を伸ばしました。")
+        return {'FINISHED'}
 
 
-# ----------------------------
+# オペレータ：ICO球生成
+class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_create_object"
+    bl_label = "ICO球生成"
+    bl_description = "ICO球を生成します"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    # メニューを実行したときに呼ばれる処理
+    def execute(self, context):
+        bpy.ops.mesh.primitive_ico_sphere_add()
+        print("ICO球を生成しました。")
+        return {'FINISHED'}
+
+
 # サブメニュークラス
-# ----------------------------
 class TOPBAR_MT_my_menu(bpy.types.Menu):
     bl_idname = "TOPBAR_MT_my_menu"
     bl_label = "MyMenu"
     bl_description = "拡張メニュー by " + bl_info["author"]
 
+    # サブメニューの描画
     def draw(self, context):
-        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+        self.layout.operator(
+            MYADDON_OT_stretch_vertex.bl_idname,
+            text=MYADDON_OT_stretch_vertex.bl_label
+        )
 
+        self.layout.operator(
+            MYADDON_OT_create_ico_sphere.bl_idname,
+            text=MYADDON_OT_create_ico_sphere.bl_label
+        )
+
+    # 既存メニューにサブメニューを追加
     def submenu(self, context):
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
 
 
-# ----------------------------
-# 登録するクラス
-# ----------------------------
+# Blenderに登録するクラスリスト
 classes = (
+    MYADDON_OT_stretch_vertex,
+    MYADDON_OT_create_ico_sphere,
     TOPBAR_MT_my_menu,
 )
 
 
-# ----------------------------
-# 有効化
-# ----------------------------
+# アドオン有効化時コールバック
 def register():
+    # Blenderにクラスを登録
     for cls in classes:
         bpy.utils.register_class(cls)
 
+    # メニューに項目を追加
     bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました。")
 
 
-# ----------------------------
-# 無効化
-# ----------------------------
+# アドオン無効化時コールバック
 def unregister():
+    # メニューから項目を削除
     bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
 
-    for cls in classes:
+    # Blenderからクラスを削除
+    for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
 
     print("レベルエディタが無効化されました。")
