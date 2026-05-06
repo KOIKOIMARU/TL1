ddb4b01f6e029896532afefd0f08fa816cdfafc0
KOIKOIMARU <k024g0108@g.neec.ac.jp>
2026-05-07 02:34:47 +0900

    01_04 メニュー追加 完了

--- level_editor.py
+++ level_editor.py
@@ -1,6 +1,6 @@
 import bpy
 
-#ブレンダーに登録するアドオン情報
+# ブレンダーに登録するアドオン情報
 bl_info = {
     "name": "レベルエディタ",
     "author": "Taro Kamata",
@@ -14,14 +14,59 @@ bl_info = {
     "category": "Object"
 }
 
-#アドオン有効化時コールバック
+# ----------------------------
+# メニュー項目（前回のやつ）
+# ----------------------------
+def draw_menu_manual(self, context):
+    self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+
+# ----------------------------
+# サブメニュークラス
+# ----------------------------
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    bl_label = "MyMenu"
+    bl_description = "拡張メニュー by " + bl_info["author"]
+
+    def draw(self, context):
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+    def submenu(self, context):
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+
+# ----------------------------
+# 登録するクラス
+# ----------------------------
+classes = (
+    TOPBAR_MT_my_menu,
+)
+
+
+# ----------------------------
+# 有効化
+# ----------------------------
 def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました。")
 
-#アドオン無効化時コールバック
+
+# ----------------------------
+# 無効化
+# ----------------------------
 def unregister():
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
     print("レベルエディタが無効化されました。")
-    
-# テスト実行用コード
+
+
+# テスト用
 if __name__ == "__main__":
     register()
